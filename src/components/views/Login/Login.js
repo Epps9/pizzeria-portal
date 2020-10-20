1e5203cf87d4ec8b454034dca877048618000c95
Ewa Witkowska <ewa.witkowska09@gmail.com>
2020-10-20 10:21:00 +0200

    Add login view

--- src/components/views/Login/Login.js
+++ src/components/views/Login/Login.js
@@ -1,10 +1,21 @@
 import React from 'react';
 //import propTypes from 'prop-types';
 import styles from './Login.module.scss';
+import { FormControl, Input, InputLabel, Paper } from '@material-ui/core';
 
 const Login = () => (
   <div className={styles.component}>
-    <h2>Login view</h2>
+    <Paper className={styles.paper}>
+      <h2>Login</h2>
+      <FormControl>
+        <InputLabel htmlFor="my-input">Email address</InputLabel>
+        <Input id="my-input" aria-describedby="my-helper-text" />
+      </FormControl> <br></br> 
+      <FormControl>
+        <InputLabel htmlFor="my-input">Password</InputLabel>
+        <Input id="my-input" aria-describedby="my-helper-text" />
+      </FormControl>  
+    </Paper>
   </div>
 );
 
